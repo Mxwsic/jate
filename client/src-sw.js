73cfeb4cd228117b9f21bb3e124e4b7abae51e8f
Mxwsic <danielapacible2@gmail.com>
2023-05-14 23:35:11 -0400

    webpack

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,8 +1,9 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
+
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
@@ -27,8 +28,7 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute(
-({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+registerRoute( ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
 new StaleWhileRevalidate({
   cacheName: 'asset-cache',
   plugins: [
